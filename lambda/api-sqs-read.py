95ca7bd339124fec54f886d16e860a0d13bbacbb
Zane DeVore <zdevore@nkywater.org>
2025-09-15 11:29:38 -0400

    initial commit

---
+++ lambda/api-sqs-read.py
@@ -0,0 +1,19 @@
+import json
+import os
+import boto3
+
+FIREHOSE = boto3.client('firehose')
+STREAM_NAME = os.environ['STREAM_NAME']
+
+def lambda_handler(event, context):
+    records = []
+    for r in event['Records']:
+        body = r['body']
+        records.append({"Data": (body if body.endswith('\n') else body + "\n").encode('utf-8')})
+    print(records)
+
+    for i in range(0, len(records), 500):
+        FIREHOSE.put_record_batch(
+            DeliveryStreamName=STREAM_NAME,
+            Records=records[i:i+500]
+        )
