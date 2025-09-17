311a68a0b39d94ce9c1b800e3cfddb03a628c98d
zane <zane.devore@gmail.com>
2025-09-17 11:35:37 -0400

    jwt-authorizer update

--- lambda/events-jwt-authorizer/lambda_function.py
+++ lambda/events-jwt-authorizer/lambda_function.py
@@ -1,20 +1,31 @@
 import json
 import jwt
 import os
+import re
 
 EXPECTED_AUDIENCE = os.environ['AUDIENCE']
 EXPECTED_ISSUER = os.environ['ISSUER']
+CLIENT_ID_REGEX = re.compile(os.environ['CLIENT_ID_REGEX'])
 
-def validate_jwt(token: str, secret: str) -> dict: 
-    claims = jwt.decode(
-        token,
-        secret,
-        audience=EXPECTED_AUDIENCE,
-        issuer=EXPECTED_ISSUER,
-        leeway=5,
-        options={"require": ["iss", "sub", "aud", "iat", "nbf", "exp"]},
-        algorithms="HS256"
-    )
+def validate_jwt(token: str, secret: str, subject: str) -> bool:
+    try:
+        claims = jwt.decode(
+            token,
+            secret,
+            audience=EXPECTED_AUDIENCE,
+            issuer=EXPECTED_ISSUER,
+            leeway=5,
+            options={"require": ["iss", "sub", "aud", "iat", "nbf", "exp"]},
+            algorithms="HS256"
+        )
+
+        if claims.get("sub") != subject:
+            raise jwt.InvalidTokenError("sub does not match")
+        
+        return True
+
+    except Exception as e:
+        return False
 
 def lambda_handler(event, context):
     # TODO implement
