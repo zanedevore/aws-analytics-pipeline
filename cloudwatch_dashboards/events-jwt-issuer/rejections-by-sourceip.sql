5da3275bf9b6f3424b77b2fef92dfb351632bf9a
Zane DeVore <zdevore@nkywater.org>
2025-09-17 09:59:00 -0400

    rejections-by-sourceip widget improvement

--- cloudwatch_dashboards/events-jwt-issuer/rejections-by-sourceip.sql
+++ cloudwatch_dashboards/events-jwt-issuer/rejections-by-sourceip.sql
@@ -1,4 +1,4 @@
-filter event = "jwt_issue" and status = "rejected"
+filter event = "jwt_issue" and status = "rejected" and ispresent(sourceIp)
 | stats count() as rejects by sourceIp
 | sort rejects desc
 | limit 20
