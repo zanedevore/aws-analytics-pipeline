78568502c4b48b640140b529e7e1af2543c2c86c
Zane DeVore <zdevore@nkywater.org>
2025-09-17 08:35:27 -0400

    folder rename and fix

--- lambda/process-sqs-messages/lambda_function.py
+++ lambda/process-sqs-messages/lambda_function.py
@@ -10,7 +10,6 @@ def lambda_handler(event, context):
     for r in event['Records']:
         body = r['body']
         records.append({"Data": (body if body.endswith('\n') else body + "\n").encode('utf-8')})
-    print(records)
 
     for i in range(0, len(records), 500):
         FIREHOSE.put_record_batch(
