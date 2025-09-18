dfadb18d76f7495ad8e4c7db7f630ff80c2636e2
zane <zane.devore@gmail.com>
2025-09-18 14:10:59 -0400

    feat: update shared secret rotation functions to accept ARN and improve error handling

--- lambda/shared-secret-rotate/lambda_function.py
+++ lambda/shared-secret-rotate/lambda_function.py
@@ -9,8 +9,10 @@ from nacl.public import PublicKey, SealedBox
 
 PUBLIC_KEY_URL = os.environ['PUBLIC_KEY_URL']
 UPDATE_SECRET_URL = os.environ['UPDATE_SECRET_URL']
+PROBE_URL = os.environ['PROBE_URL']
 API_KEY_ID = os.environ['API_KEY_ID']
-SHARED_SECRET_ID = os.environ['SHARED_SECRET_ID']
+TEST_CLIENT_ID = os.environ['TEST_CLIENT_ID']
+TEST_AUDIENCE = os.environ['TEST_AUDIENCE']
 
 _secrets = boto3.client('secretsmanager')
 _base_header = {
@@ -31,9 +33,9 @@ def encrypt_secret(message: bytes, public_key_b64: str) -> str:
 
     return base64.b64encode(ct).decode()
 
-def create_secret(token: str) -> dict | None:
+def create_secret(token: str, arn: str) -> dict | None:
     try:
-        _secrets.get_secret_value(SecretId=SHARED_SECRET_ID, VersionStage="AWSPENDING")
+        _secrets.get_secret_value(SecretId=arn, VersionID=token, VersionStage="AWSPENDING")
         return {'statusCode': 500, 'body': 'Pending secret already exists'}
     except _secrets.exceptions.ResourceNotFoundException:
         pass
@@ -41,7 +43,7 @@ def create_secret(token: str) -> dict | None:
     new_secret = secrets.token_urlsafe(32)
 
     _secrets.put_secret_value(
-        SecretId = SHARED_SECRET_ID,
+        SecretId = arn,
         ClientRequestToken = token,
         SecretString = new_secret,
         VersionStages = ['AWSPENDING']
@@ -62,30 +64,49 @@ def set_secret(encrytped_secret_b64: str) -> None:
     resp.raise_for_status()
 
 
-def test_secret():
-    pass
+def test_secret(arn: str) -> dict:
+    secret = _secrets.get_secret_value(SecretId=arn, VersionStage="AWSPENDING")['SecretString']
+    body = {
+        "client_id": TEST_CLIENT_ID,
+        "client_secret": secret,
+        "audience": TEST_AUDIENCE
+    }
 
-def finish_secret():
-    pass
+    token_response = requests.post(
+        PROBE_URL, 
+        json=body,
+        headers={"Content-Type": "application/json"}
+    )
+    token_response.raise_for_status()
+    return {"message": "testSecret succeeded"}
+
+def finish_secret(token: str, arn: str) -> dict:
+    meta = _secrets.describe_secret(SecretId=arn)
+    current_version = None
+    for v, stages in meta['VersionIdsToStages'].items():
+        if 'AWSCURRENT' in stages:
+            current_version = v
+            break
+    
+    _secrets.update_secret_version_stage(
+        SecretId = arn,
+        VersionStage = "AWSCURRENT",
+        MoveToVersionId = token,
+        RemoveFromVersionId = current_version
+    )
+
+    return {"message": "finishSecret complete"}
 
 def lambda_handler(event, context):
-    # Create secret, set secret, test secret, finish secret
     step = event['step']
     token = event['ClientRequestToken']
+    arn = event.get('SecretId')
 
     if step == 'createSecret':
         return create_secret(token)
     elif step == 'setSecret':
-        return set_secret(_secrets.get_secret_value(SecretId=SHARED_SECRET_ID, VersionStage="AWSPENDING")['SecretString'])
+        return set_secret(_secrets.get_secret_value(SecretId=arn, VersionStage="AWSPENDING")['SecretString'])
     elif step == 'testSecret':
-        return test_secret()
+        return test_secret(arn)
     elif step == 'finishSecret':
-        return finish_secret()
-
-    
-
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+        return finish_secret(token, arn)
