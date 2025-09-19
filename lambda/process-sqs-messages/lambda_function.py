4fc53b772cbc2da7f97a2a8d77d6fe6773204901
zane <zane.devore@gmail.com>
2025-09-19 08:24:36 -0400

    fix: add logging to lambda_handler for record processing in Firehose

--- lambda/process-sqs-messages/lambda_function.py
+++ lambda/process-sqs-messages/lambda_function.py
@@ -1,6 +1,9 @@
-import json
 import os
 import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 FIREHOSE = boto3.client('firehose')
 STREAM_NAME = os.environ['STREAM_NAME']
@@ -10,6 +13,8 @@ def lambda_handler(event, context):
     for r in event['Records']:
         body = r['body']
         records.append({"Data": (body if body.endswith('\n') else body + "\n").encode('utf-8')})
+    
+    logger.info(f"Sending {len(records)} records to Firehose")
 
     for i in range(0, len(records), 500):
         FIREHOSE.put_record_batch(
